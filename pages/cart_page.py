68c4bb7134559c9c45e2d90744d819f2e5783ec2
Djon <lioni767@yandex.ru>
2026-06-03 00:35:26 +0300

    move locators and steps from test_cart to CartPage (fix curator point 8)

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,12 +1,16 @@
 from selenium.webdriver.common.by import By
 import allure
 
+
 class CartPage:
     def __init__(self, driver):
         self.driver = driver
 
     CHECKOUT_BUTTON = (By.ID, 'checkout')
     CART_ITEM = (By.CLASS_NAME, 'cart_item')
+    CART_BADGE = (By.CLASS_NAME, 'shopping_cart_badge')
+    REMOVE_BUTTON = (By.XPATH, '//button[text()="Remove"]')
+    CONTINUE_SHOPPING_BUTTON = (By.ID, 'continue-shopping')
 
     @allure.step('Нажать кнопку Checkout')
     def click_checkout(self):
@@ -18,3 +22,25 @@ class CartPage:
         items = self.driver.find_elements(*self.CART_ITEM)
         assert len(items) == expected_count
         return self
+
+    @allure.step("Проверить, что счетчик корзины показывает {expected_count}")
+    def should_have_cart_count(self, expected_count: str):
+        cart_count = self.driver.find_element(*self.CART_BADGE)
+        assert cart_count.text == expected_count
+
+    @allure.step("Проверить, что корзина пуста")
+    def should_be_empty(self):
+        cart_items = self.driver.find_elements(*self.CART_ITEM)
+        assert len(cart_items) == 0
+
+    @allure.step("Удалить первый товар из корзины")
+    def remove_first_item(self):
+        self.driver.find_element(*self.REMOVE_BUTTON).click()
+
+    @allure.step("Нажать Continue Shopping")
+    def click_continue_shopping(self):
+        self.driver.find_element(*self.CONTINUE_SHOPPING_BUTTON).click()
+
+    @allure.step("Проверить, что открыта страница каталога")
+    def should_be_on_inventory_page(self):
+        assert 'inventory.html' in self.driver.current_url
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,7 +1,7 @@
 import allure
-from selenium.webdriver.common.by import By
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
+from pages.cart_page import CartPage
 
 
 @allure.epic('UI тестирование')
@@ -14,46 +14,40 @@ class TestCart:
     def test_add_item_to_cart(self, driver):
         login_page = LoginPage(driver)
         inventory_page = InventoryPage(driver)
+        cart_page = CartPage(driver)
 
         login_page.open()
         login_page.login('standard_user', 'secret_sauce')
         inventory_page.add_first_item_to_cart()
 
-        with allure.step('Проверить, что товар добавлен (счетчик корзины = 1)'):
-            cart_count = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge')
-            assert cart_count.text == '1'
+        cart_page.should_have_cart_count('1')
 
     @allure.story('Удаление товара')
     @allure.severity(allure.severity_level.NORMAL)
     def test_remove_item_from_cart(self, driver):
         login_page = LoginPage(driver)
         inventory_page = InventoryPage(driver)
+        cart_page = CartPage(driver)
 
         login_page.open()
         login_page.login('standard_user', 'secret_sauce')
         inventory_page.add_first_item_to_cart()
         inventory_page.open_cart()
 
-        with allure.step('Удалить товар из корзины'):
-            driver.find_element(By.XPATH, '//button[text()="Remove"]').click()
-
-        with allure.step('Проверить, что корзина пуста'):
-            cart_items = driver.find_elements(By.CLASS_NAME, 'cart_item')
-            assert len(cart_items) == 0
+        cart_page.remove_first_item()
+        cart_page.should_be_empty()
 
     @allure.story('Продолжение покупок')
     @allure.severity(allure.severity_level.MINOR)
     def test_continue_shopping(self, driver):
         login_page = LoginPage(driver)
         inventory_page = InventoryPage(driver)
+        cart_page = CartPage(driver)
 
         login_page.open()
         login_page.login('standard_user', 'secret_sauce')
         inventory_page.add_first_item_to_cart()
         inventory_page.open_cart()
 
-        with allure.step('Нажать кнопку Continue Shopping'):
-            driver.find_element(By.ID, 'continue-shopping').click()
-
-        with allure.step('Проверить, что вернулись на страницу товаров'):
-            assert 'inventory.html' in driver.current_url
+        cart_page.click_continue_shopping()
+        cart_page.should_be_on_inventory_page()
